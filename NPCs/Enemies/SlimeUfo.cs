75823e36f3d26d4952911964718846ee11d900b8
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-10-01 19:31:43 -0300

    H

--- NPCs/Enemies/SlimeUfo.cs
+++ NPCs/Enemies/SlimeUfo.cs
@@ -6,7 +6,7 @@ using Terraria.ModLoader;
 
 namespace SpaceModjam.NPCs.Enemies
 {
-    class SlimeUfo : ModNPC
+    public class SlimeUfo : ModNPC
     {
         bool shootAttack = false;
         public override void SetStaticDefaults()
@@ -15,27 +15,30 @@ namespace SpaceModjam.NPCs.Enemies
         }
         public override void SetDefaults()
         {
+			npc.noGravity = true;
+            npc.noTileCollide = true;
+			npc.netAlways = true;
             npc.width = 64;
             npc.height = 46;
             npc.damage = 20;
             npc.lifeMax = 250;
             npc.defense = 15;
-            npc.knockBackResist = 0.5f;
-            npc.value = 100;
             npc.aiStyle = -1;
-            npc.npcSlots = 1;
-            npc.noGravity = true;
-            npc.noTileCollide = true;
+			aiType = -1;
+			npc.knockBackResist = 0.5f;
+            npc.npcSlots = 1f;
             npc.HitSound = SoundID.NPCHit4;
             npc.DeathSound = SoundID.NPCDeath14;
+			npc.value = Item.sellPrice();
         }
+		
         public override void AI()
         {
             npc.TargetClosest(true);
-            if (!shootAttack) npc.ai[0]++;
+            if (!shootAttack) 
+				npc.ai[0]++;
             Player player = Main.player[npc.target];
             Vector2 target = npc.HasPlayerTarget ? player.Center : Main.npc[npc.target].Center;
-            npc.netAlways = true;
             if (npc.ai[0] >= 60 && Math.Abs(target.X - npc.Center.X) < 5  && npc.Center.Y <= target.Y)
             {
                 shootAttack = true;
