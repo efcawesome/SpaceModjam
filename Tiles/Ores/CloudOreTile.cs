478d9a188bc5f39c47664a20acd3cf91e1f445d7
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-10-01 19:29:07 -0300

    H

---
+++ Tiles/Ores/CloudOreTile.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using SpaceModjam.Items.Materials;
+
+namespace SpaceModjam.Tiles.Ores
+{
+	public class CloudOreTile : ModTile
+	{
+		public override void SetDefaults()
+		{
+			TileID.Sets.Ore[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileSpelunker[Type] = true; 
+			Main.tileShine2[Type] = true; 
+			Main.tileValue[Type] = 410; 
+			Main.tileShine[Type] = 975; 
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Cloud Ore");
+			AddMapEntry(new Color(152, 171, 198), name);
+
+			dustType = 84;
+			drop = ModContent.ItemType<CloudOre>();
+			soundType = SoundID.Tink;
+			soundStyle = 1;
+			mineResist = 2f;
+			minPick = 55;
+		}
+	}
+}
